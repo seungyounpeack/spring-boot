6fd9a5d02d13acc827ef7e28fa3646f8ce6a9d17
seungyounpeack <bsy920521@naver.com>
2025-03-18 10:45:24 +0900

    프로젝트 세팅 및 config설정

--- OAuth2/src/main/java/com/example/OAuth2/OAuth2Application.java
+++ OAuth2/src/main/java/com/example/OAuth2/OAuth2Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.OAuth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class OAuth2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(OAuth2Application.class, args);
 	}
 
 }
